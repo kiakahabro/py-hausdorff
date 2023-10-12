f97571df991fd5b7f6b21b3885a7b6e8e6669998
Tim Farnworth <timothy.farnworth@uon.edu.au>
2023-10-12 13:06:51 +1100

    Use black formating

--- tests/distances_test.py
+++ tests/distances_test.py
@@ -1,23 +1,29 @@
-import pytest
 import numpy as np
+import pytest
+
 from hausdorff.hausdorff import hausdorff_distance
+
 
 @pytest.fixture
 def input_arrays():
     np.random.seed(42)
-    XA = np.random.random((1000,5))
-    XB = np.random.random((2000,5))
-    return (XA,XB)
+    XA = np.random.random((1000, 5))
+    XB = np.random.random((2000, 5))
+    return (XA, XB)
 
-@pytest.mark.parametrize("distance", [
-    "manhattan",
-    "euclidean",
-    "chebyshev",
-    "cosine",
-    "haversine",
-])
+
+@pytest.mark.parametrize(
+    "distance",
+    [
+        "manhattan",
+        "euclidean",
+        "chebyshev",
+        "cosine",
+        "haversine",
+    ],
+)
 def test_it_computes_haussdorf_distance(input_arrays, distance):
-    XA,XB = input_arrays
+    XA, XB = input_arrays
 
     actual_dist = hausdorff_distance(XA, XB, distance=distance)
 
